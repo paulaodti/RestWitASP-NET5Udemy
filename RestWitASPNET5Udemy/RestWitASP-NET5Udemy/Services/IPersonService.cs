7011c8c98399c5417a058400e1da68e0746c8ce0
paulo.teixeira <pauloandradeteixeira@gmail.com>
2020-12-08 16:07:20 -0300

    Criando a interface Person e os métodos

---
+++ RestWitASPNET5Udemy/RestWitASP-NET5Udemy/Services/IPersonService.cs
@@ -0,0 +1,18 @@
+﻿using RestWitASP_NET5Udemy.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWitASP_NET5Udemy.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindByID(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+
+    }
+}
---
+++ RestWitASPNET5Udemy/RestWitASP-NET5Udemy/Services/Implementations/PersonServiceImplementation.cs
@@ -0,0 +1,52 @@
+﻿using RestWitASP_NET5Udemy.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWitASP_NET5Udemy.Services.Implementations
+{
+    public class PersonServiceImplementation : IPersonService
+    {
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+        }
+
+        public List<Person> FindAll()
+        {
+            var persons = new List<Person>();
+            for (int i = 0; i<=8; i++)
+            {
+                persons.Add(MockPerson(i));
+            };
+            return persons;
+        }
+
+        public Person FindByID(long id)
+        {
+            return MockPerson(Convert.ToInt32(id));
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+
+        public Person MockPerson(int i)
+        {
+            return new Person
+            {
+                Id = i,
+                FirstName = "Paulo Henrique " + i,
+                Address = "Rua Monte Castelo Branco, Cambui, Minas Gerais",
+                Gender = "Male",
+                LastName = "Andrade Teixeira"
+            };
+        }
+    }
+}
