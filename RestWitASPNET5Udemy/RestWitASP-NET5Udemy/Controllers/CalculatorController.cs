a2b1e441caa9b855e1f26b796ad390ceff1ef7c3
paulo.teixeira <pauloandradeteixeira@gmail.com>
2020-12-08 15:12:26 -0300

    Add as novas operações a nossa calculadora

--- RestWitASPNET5Udemy/RestWitASP-NET5Udemy/Controllers/CalculatorController.cs
+++ RestWitASPNET5Udemy/RestWitASP-NET5Udemy/Controllers/CalculatorController.cs
@@ -20,26 +20,94 @@ namespace RestWitASP_NET5Udemy.Controllers
         }
 
         [HttpGet("sum/{firstNumber}/{secondNumber}")]
-        public IActionResult Get(string firstNumber, string secondNumber)
+        public IActionResult Soma(string firstNumber, string secondNumber)
         {
             if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
             {
-                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                var sum = ConvertToDouble(firstNumber) + ConvertToDouble(secondNumber);
                 return Ok(sum.ToString());
 
             }
             return BadRequest("Invalid Input");
         }
 
-        private decimal ConvertToDecimal(string srtNumber)
+        [HttpGet("sub/{firstNumber}/{secondNumber}")]
+        public IActionResult Subtracao(string firstNumber, string secondNumber)
         {
-            return Decimal.Parse(srtNumber);
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDouble(firstNumber) - ConvertToDouble(secondNumber);
+                return Ok(sum.ToString());
+
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet("div/{firstNumber}/{secondNumber}")]
+        public IActionResult Divisao(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber) && ConvertToDouble(secondNumber) != 0)
+            {
+                var sum = ConvertToDouble(firstNumber)/ConvertToDouble(secondNumber);
+                return Ok(sum.ToString());
+
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet("multi/{firstNumber}/{secondNumber}")]
+        public IActionResult Multiplicacao(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDouble(firstNumber) * ConvertToDouble(secondNumber);
+                return Ok(sum.ToString());
+
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet("avg/{firstNumber}/{secondNumber}")]
+        public IActionResult Media(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = (ConvertToDouble(firstNumber) + ConvertToDouble(secondNumber))/2;
+                return Ok(sum.ToString());
+
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet("sqrt/{firstNumber}")]
+        public IActionResult RaizQuarada(string firstNumber)
+        {
+            if (IsNumeric(firstNumber))
+            {
+                double number = ConvertToDouble(firstNumber);
+                if (IsNaturalNumber(number))
+                {
+                    var sum = Math.Sqrt(ConvertToDouble(firstNumber));
+                    return Ok(sum.ToString());
+                }
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        private double ConvertToDouble(string srtNumber)
+        {
+            return Double.Parse(srtNumber);
         }
 
         private bool IsNumeric(string srtNumber)
         {
             double number;
             return double.TryParse(srtNumber, System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out number);
+        }
+
+        private bool IsNaturalNumber(double number)
+        {
+            return number >= 0;    
         }
     }
 }
